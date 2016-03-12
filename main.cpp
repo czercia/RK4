83b68da33b743f334f8cc9ecee9491dc1ce32123
Marta <m.sroczynska@student.uw.edu.pl>
2016-03-12 21:23:41 +0100

    Cleaned code.

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    Parameters params1(0, 10, 0.0005, 60, 0); //t0 tmax dt L 0-dirichlet/1-periodic
+    Parameters params1(0, 5, 0.0005, 60, 0); //t0 tmax dt L 0-dirichlet/1-periodic
     RK4::solve(params1);
 
     return 0;
